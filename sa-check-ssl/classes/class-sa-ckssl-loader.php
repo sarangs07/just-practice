5a099b7ae5b49e42fa1ade57b34967d816dfe6e9
Sarang <sarangs@bsf.io>
2018-08-08 17:50:22 +0530

    [Update] -> Plugin code updated

--- sa-check-ssl/classes/class-sa-ckssl-loader.php
+++ sa-check-ssl/classes/class-sa-ckssl-loader.php
@@ -24,14 +24,45 @@
      		
      		function __construct()
      		{
-     			$this->checkSsl();
+     			
+                
+                $this->get_domain();
      		}
 
-     		function checkSsl(){
-     			$web_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-     			
-     			echo "<script>alert('".$web_link."');</script>";
-     		}
+     		
+
+
+               function get_domain()
+               {
+                     //echo "in the function";
+                     $uri = $_SERVER['REQUEST_URI'];
+                    //echo $uri."<br />"; // Outputs: URI
+                     
+                    $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+                     
+                    $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+                    //echo "Website URL is : ".$url."<br />"; // Outputs: Full URL
+                     
+                    $query = $_SERVER['QUERY_STRING'];
+                    //echo $query."<br />"; // Outputs: Query String
+
+                    $parse = parse_url($url);
+                    //echo "Your Domain Name is :".$parse['host'];
+
+                    switch ($protocol) {
+                        case 'https://':
+                            echo "Your website is HTTPS enabled";
+                            break;
+                        case 'http://':
+                            echo "Your website is not HTTPS enabled";
+                            break;
+                        default:
+                            echo "Nothing found";
+                            break;
+                    }
+               }
+
+               
      	}
 
      	$obj = new Sa_Check_Ssl();
